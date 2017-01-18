12520cf6007a53d08dbe9862c6d404b204bc12a8
Somesh Singh <somesh.singh1992@gmail.com>
2017-01-18 14:39:04 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php 
+ // including the helper functions
+ require('view/header.php'); ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+	<div class="col-sm-3 col-lg-4">
+	<img src="img/me.jpg"  class="img-responsive" alt="Somesh's pic"> 
+        </div>
+            <div class="col-sm-9 col-lg-8">
+ <!--           
+                <h1>Starting with BootStrap</h1>  
+                <p class="lead"></p>
+                <ul class="list-unstyled">
+                    <li>Bootstrap v3.3.7</li>
+                    <li>jQuery v1.11.1</li>
+                </ul>
+ -->
+        <p>
+	Welcome! I am a third year Ph.D. student at the department of 
+<a href="http://www.cse.iitm.ac.in/" target="_blnk">
+Computer Science and Engineering 
+</a>
+at 
+<a href="http://www.iitm.ac.in/" target="_blnk">
+IIT Madras</a>. 
+I am advised by 
+<a href = "http://www.cse.iitm.ac.in/~rupesh/" target="_blank">Dr. Rupesh Nasre</a>. 
+My research focuses on designing approximation techniques for parallel graph algorithms on GPUs.  
+        </p>
+	 <p>
+        I graduated from 
+<a href = "http://nituk.ac.in/" target="_blank">NIT Uttarakhand</a> 
+in 2014 with a B.Tech. in Computer Science and Engineering.
+        </p>
+
+	<hr />
+
+	<h3>Contact:</h3>
+
+	<p>
+	  Somesh Singh <br/>
+	<a href = "http://pace.cse.iitm.ac.in/" target="_blank"> PACE Lab</a>,
+	  BSB 331-A <br/>
+	  Dept. of CSE<br/>
+	  IIT Madras - 600 036.<br/>
+	  Ph: +91 9791264972<br/>
+	  Email: somesh&#46;singh1992&#64;gmail&#46;com<br/>
+	</p>
+
+        </div>
+      
+
+        </div>
+        <!-- /.row -->
+<hr />
+
+<div align="right">
+    <a href="https://www.linkedin.com/in/somesh-singh-17aab053" target="_blank">
+
+     <img
+          src="img/btn_linkedin_160x25.png"
+          width="160" height="25"  border="0" alt="View Somesh Singh's profile on LinkedIn">
+
+    </a>
+
+</div>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery Version 1.11.1 -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
+
+
+
